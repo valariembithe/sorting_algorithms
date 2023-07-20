396655182852190c20c16656cc0b6436db42ec6c
valariembithe <valmbithe691@gmail.com>
2023-07-20 05:18:04 -0700

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ *  order using the Insertion sort algorithm
+ * @list: a doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+
+/**
+ * @swap_node - interchanges a node for a previous one
+ * @node: node
+ * @list: node list
+ *
+ * Return: a pointer to a node which was entered
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
